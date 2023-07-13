6a27f6212305c856e17d83c94a61e3dc6c1589dc
victoriaprisco <p.victoria@wustl.edu>
2023-07-13 12:56:53 -0500

    added code to make a new split screen window

--- features/extension.js
+++ features/extension.js
@@ -17,11 +17,12 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('features.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('features.newWindow', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from features!');
+		const panel = vscode.window.createWebviewPanel('windowName', 'New Window Display Name', vscode.ViewColumn.Two, {});
+		panel.webview.html = "<h1> hardcode html here </h1>";
 	});
 
 	context.subscriptions.push(disposable);
---
+++ features/newwinddow.md
@@ -0,0 +1,3 @@
+documentation at: https://code.visualstudio.com/api/extension-guides/webview
+
+webview panels are returned as references, if the reference is lost then the panel cannot be opened. the same reference must be maintained to edit and manage the panel.
